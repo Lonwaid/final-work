f1bb7134d3ba05d6e0babf1e5395892f13992ffc
Lonwaid <Andrey9053@yandex.ru>
2019-10-24 22:49:51 +0300

    Сделан адаптив для секции navbar, добавлено бургер-меню

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('.burger-button').on('click', function(){
+		$(this).toggleClass('burger-button_dissabled');
+		$('.burger-menu__wrapper').toggleClass('burger-menu__wrapper_enabled');
+		$('.navbar__logo').toggleClass('navbar__logo_margin');
+	});
+	$('.burger-button__close').on('click', function(){
+		$('.burger-menu__wrapper').removeClass('burger-menu__wrapper_enabled');	
+		$('.navbar__logo').removeClass('navbar__logo_margin');
+		$('.burger-button').removeClass('burger-button_dissabled');
+	});
+});
